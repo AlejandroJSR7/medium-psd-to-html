6b4a5c82400b004346a51cc0d1bc62cdd4096d08
ja5054r <ja5054r@gmail.com>
2015-04-12 01:26:21 -0500

    Parallax and more

--- js/medium.js
+++ js/medium.js
@@ -4,3 +4,26 @@
 	ja5054r@gmail.com
 	ja5054r
 */
+
+$('.header_image').parallax({imageSrc: 'images/slider/principal-1.jpg'});
+$('.share-article').parallax({imageSrc: 'images/design/bg/bg-footer.jpg'});
+
+
+$(document).ready(function(){
+	
+	//Check to see if the window is top if not then display button
+	$(window).scroll(function(){
+		if ($(this).scrollTop() > 100) {
+			$('.backToTop').fadeIn();
+		} else {
+			$('.backToTop').fadeOut();
+		}
+	});
+	
+	//Click event to scroll to top
+	$('.backToTop').click(function(){
+		$('html, body').animate({scrollTop : 0},800);
+		return false;
+	});
+	
+});
